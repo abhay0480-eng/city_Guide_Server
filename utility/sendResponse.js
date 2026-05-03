bb40d45dab412534199dea5beaa491dcba7a9ef3
Abhay Kumar <abhay0480sharma@gmail.com>
2026-05-03 17:35:53 +0530

    add allow origin

--- utility/sendResponse.js
+++ utility/sendResponse.js
@@ -2,5 +2,7 @@
 export const sendResponse = (res, statusCode, contentType, payload) => {
     res.statusCode = statusCode
     res.setHeader('Content-Type', contentType)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET')
     res.end(payload)
 }
